2b75f364f004f5c00b5f0580e66b284708118332
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-02-25 22:58:52 -0500

    change(types): add account-related types

--- libs/types/src/account.d.ts
+++ libs/types/src/account.d.ts
@@ -1,1 +1,33 @@
+import { AssetId, Balance, FileLike, FileLocation } from "./primitives.js";
+
 export type AccountId = string | number[];
+
+export type Account<File extends FileLike = FileLocation> = {
+  id: AccountId;
+  identity?: AccountIdentity;
+  balance: AccountBalance;
+  assets: Record<AssetId, AssetBalance>;
+};
+
+export type AccountBalance = {
+  free: Balance;
+  reserved: Balance;
+  frozen: Balance;
+};
+
+export type AssetBalance = {
+  assetId: AssetId;
+  balance: Balance;
+};
+
+export type AccountIdentity = {
+  display: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email?: string;
+  image?: File;
+  additional: IdentityAdditionalField[];
+};
+
+export type IdentityAdditionalField = [string, string];
